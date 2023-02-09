e52c274de3bb781ba5606012e29b0e465d624df4
LeAnGel <angel.pozo.miranda@hotmail.com>
2023-02-09 15:32:55 +0100

    second

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+
+$author = "Cervantes";
+$userName = "@Cervantes";
+$twit = "En un lugar de la Mancha de cuyo nombre no quiero acordarme...";
+$image = "https://mcn-images.bauersecure.com/wp-images/3688/1440x960/08suzuki-gsx-r600.jpg?mode=max&quality=90&scale=down";
+
+require_once "index.html";
+
